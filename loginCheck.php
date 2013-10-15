e7fccc1a79bf05fa49aaef34e25982df81cb729d
Alain Beauseigle <abeauseigle@videoton.ca>
2013-10-14 20:43:46 -0400

    Code clean up
    Code clean up

--- loginCheck.php
+++ loginCheck.php
@@ -1,44 +1,22 @@
 <?php
-/*
-$dbhost = "localhost";
-$dbuname = "root";
-$dbpass = "";
-$dbname = "test";
-*/
-$dbhost = "localhost";
-$dbname = "_________";
-$dbuname = "_________";
-$dbpass = "_________";
+//To test, use: http://www.affairesup.com/webSqlApp/loginCheck.php?username=exp&password=4success
+require_once('connections/connDb.php');
+$CLnum_rows = 0;
+$strUname = "";
+$strPword = "";
+$strUname = (isset($_POST["username"])) ? $_POST["username"] : NULL;
+$strPword = (isset($_POST["password"])) ? $_POST["password"] : NULL;
 
-$connect=mysql_pconnect($dbhost, $dbuname, $dbpass) or die("Impossible de se connecter au serveur $server"); 
-$db= mysql_select_db($dbname);
+echo "<p>username is ", $strUname, " password is ", $strPword, "</p>";
 
-mysql_set_charset('utf8', $connect);		
-/* De Tuanan Android: expertup/mobile/checkLogin.php
-$strUname = htmlentities(   (  (get_magic_quotes_gpc()  ) ? $_REQUEST['UsagerNom'] : addslashes($_REQUEST['UsagerNom']))   , ENT_QUOTES);
-$strPword = htmlentities(   (  (get_magic_quotes_gpc()  ) ? $_REQUEST['UsagerPassword'] : addslashes($_REQUEST['UsagerPassword']))   , ENT_QUOTES);
-*/
-
-// À faire: Prendre le userId et le password de la table userParam de la BD webSQL (comment en js ? via un login.html bidon?)
-//En attendant, le ID et PW est hardcodé pour vérifier la fonctionnalité de webSqlSync.js 
-$loginPost = stripslashes('__my_user_name__'); //ToDo: To use the user name and password of the WebSqlApp stored in the webSql DB
-$passwordPost = stripslashes('__my_password__'); 
-
-$strUname = mysql_real_escape_string($loginPost); 
-$strPword = mysql_real_escape_string($passwordPost); 
-	 
-// CL est pour Check Login
-$CLquery = "SELECT Count(UsagerID) FROM RN_Usager"
+// CL is for Check Login
+$CLquery = "SELECT * FROM Usager"
 				 	 ." WHERE UsagerActif = 1 AND UsagerNom = '".$strUname."'"
 					 ." AND UsagerPassword = PASSWORD('".$strPword."') "
 					 ." ORDER BY UsagerID DESC";
 
 $CLresult = mysql_query($CLquery) or die(mysql_error());
 $CLnum_rows = mysql_num_rows($CLresult);
-
-session_start();	//pas présent dans le code de Android ??? peut-être à cause qu'il ne faut pas de variable de session ?  // possiblement à mettre plus haut 
-$_SESSION['ClientID']=$CLresult['ClientID'];
-
 ?>
 
 
